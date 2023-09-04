e6e4f59087fb5c85d3ec94c88095d1fac24ec21c
Yilin Yang <yyl_20050115@hotmail.com>
2023-09-05 03:03:04 +0800

    fx

--- Chapter03/DTRFSample/Program.cs
+++ Chapter03/DTRFSample/Program.cs
@@ -10,34 +10,33 @@ using SharpLearning.InputOutput.Csv;
 using SharpLearning.Metrics.Regression;
 using SharpLearning.RandomForest.Learners;
 
-namespace DTRFSample
+namespace DTRFSample;
+
+public class Program
 {
-    class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.ReadKey();
-            var parser = new CsvParser(() => new StringReader(Resources.Glass));
-            var observations = parser.EnumerateRows(v => v != "Target").ToF64Matrix();
-            var targets = parser.EnumerateRows("Target").ToF64Vector();
-            var rows = targets.Length;
-            int trees = 100;
-            double subSample = 1.0;
+        Console.ReadKey();
+        var parser = new CsvParser(() => new StringReader(Resources.Glass));
+        var observations = parser.EnumerateRows(v => v != "Target").ToF64Matrix();
+        var targets = parser.EnumerateRows("Target").ToF64Vector();
+        var rows = targets.Length;
+        int trees = 100;
+        double subSample = 1.0;
 
-            var sut = new RegressionExtremelyRandomizedTreesLearner(trees, 1, 100, 1, 0.0001, subSample, 42, false);
+        var sut = new RegressionExtremelyRandomizedTreesLearner(trees, 1, 100, 1, 0.0001, subSample, 42, false);
 
-            var indices = Enumerable.Range(0, targets.Length).ToArray();
-            indices.Shuffle(new Random(42));
-            indices = indices.Take((int)(targets.Length * 0.7)).ToArray();
+        var indices = Enumerable.Range(0, targets.Length).ToArray();
+        indices.Shuffle(new Random(42));
+        indices = indices.Take((int)(targets.Length * 0.7)).ToArray();
 
-            var model = sut.Learn(observations, targets, indices);
+        var model = sut.Learn(observations, targets, indices);
 
-            var predictions = model.Predict(observations);
+        var predictions = model.Predict(observations);
 
-            var evaluator = new MeanSquaredErrorRegressionMetric();
-            var error = evaluator.Error(targets, predictions);
-            Console.WriteLine("Error: " + error);
-            Console.ReadKey();
-        }
+        var evaluator = new MeanSquaredErrorRegressionMetric();
+        var error = evaluator.Error(targets, predictions);
+        Console.WriteLine("Error: " + error);
+        Console.ReadKey();
     }
 }
